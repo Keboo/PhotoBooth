084f0846bfb559a7a24940977d9d279866ae2b5e
Kevin Bost <kitokeboo@gmail.com>
2023-07-20 01:02:06 -0700

    Maybe an idea

--- PhotoBooth/MainWindowViewModel.cs
+++ PhotoBooth/MainWindowViewModel.cs
@@ -1,36 +1,55 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Windows.Media.Imaging;
+
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+
+using OpenCvSharp;
+using OpenCvSharp.WpfExtensions;
 
 namespace PhotoBooth;
 
 public partial class MainWindowViewModel : ObservableObject
 {
-    //This is using the source generators from CommunityToolkit.Mvvm to generate a RelayCommand
-    //See: https://learn.microsoft.com/dotnet/communitytoolkit/mvvm/generators/observableproperty
-    //and: https://learn.microsoft.com/windows/communitytoolkit/mvvm/observableobject
     [ObservableProperty]
-    [NotifyCanExecuteChangedFor(nameof(IncrementCountCommand))]
-    private int _count;
+    private WriteableBitmap? _image;
     
     public MainWindowViewModel()
     {
-        
     }
 
     //This is using the source generators from CommunityToolkit.Mvvm to generate a RelayCommand
     //See: https://learn.microsoft.com/dotnet/communitytoolkit/mvvm/generators/relaycommand
     //and: https://learn.microsoft.com/windows/communitytoolkit/mvvm/relaycommand
-    [RelayCommand(CanExecute = nameof(CanIncrementCount))]
-    private void IncrementCount()
+    [RelayCommand(IncludeCancelCommand = true)]
+    private async Task StartCamera(CancellationToken token)
     {
-        Count++;
-    }
+        try
+        {
+            using VideoCapture capture = VideoCapture.FromCamera(1);
+            capture.FrameWidth = 1920;
+            capture.FrameHeight = 1080;
+            using Mat image = new();
+            while (!token.IsCancellationRequested)
+            {
+                capture.Read(image);
+                if (image.Empty())
+                    break;
 
-    private bool CanIncrementCount() => Count < 5;
+                if (Image is null)
+                {
+                    Image = image.ToWriteableBitmap();
+                }
+                else
+                {
+                    WriteableBitmapConverter.ToWriteableBitmap(image, Image);
+                }
 
-    [RelayCommand]
-    private void ClearCount()
-    {
-        Count = 0;
+                await Task.Delay(10, token);
+            }
+        }
+        catch(Exception e)
+        {
+
+        }
     }
 }
